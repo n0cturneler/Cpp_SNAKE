7c5ba05153fbcffcfede26152524219472aa7ee0
n0cturneler <shizucl0ak@gmail.com>
2026-04-30 16:54:39 +0800

    starter

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,36 @@
+#include "Profiler.h"
+
+#include <SFML/Graphics.hpp>
+
 #include <iostream>
+#include <optional>
 
 int main()
 {
 	std::cout << "Hello, World!";
+
+	sf::RenderWindow window(sf::VideoMode({800, 800}), "TITLE");
+
+	sf::CircleShape	shape(100.0);
+
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+
+		while (const std::optional event = window.pollEvent())
+		{
+			if (event->is<sf::Event::Closed>())
+			{
+				window.close();
+			}
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+
+	}
 
 	return 0;
 }
